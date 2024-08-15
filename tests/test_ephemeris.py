b0c10b3f474f506c4d4cf64f7b550f859d3d425a
AdrienneKirk <adriennemlkirk@gmail.com>
2024-08-15 13:56:34 -0600

    precession tests

--- tests/test_ephemeris.py
+++ tests/test_ephemeris.py
@@ -27,6 +27,22 @@ test_T0_err_quad = 0.3467430587812461#conjunction time err quad
 test_dPdE =  4.223712653342504e-06#period change by epoch
 test_dPdE_err = 7.742732700893123e-06#period change by epoch error
 
+test_epochs_precession = np.array([ -1640, -1346,  -1342, -1067, -1061, -1046,  -1038])
+test_mtts_precession = np.array([2454515.525,2454836.403,2454840.769,2455140.91, 2455147.459, 2455163.831,2455172.561])
+test_mtts_err_precession = np.array([0.00043, 0.00028, 0.00062, 0.00042, 0.00043, 0.00032, 0.00036])
+test_tra_or_occ_precession = np.array(['tra','occ','occ','tra', 'tra', 'tra', 'tra'])
+test_tra_or_occ_enum = [0 if i == 'tra' else 1 for i in test_tra_or_occ_precession]
+test_P_pre =  1.0914233780823739
+test_P_err_pre =  2.5837552101593316e-06
+test_T0_pre =  2454515.5247473116
+test_T0_err_pre =  0.0016189004920040013
+test_dwdE =  -862310.36579702
+test_dwdE_err = 1933.7369753292073
+test_e =  0.5345414535549522
+test_e_err = 13588.186664524692
+test_w =  -55020653.47561098
+test_w_err= 589049.1819169023
+
 test_observed_data = test_mtts
 test_uncertainties= test_mtts_err    
 
@@ -46,8 +62,10 @@ class TestLinearModelEphemeris(unittest.TestCase):
             NOTE: The last number should round to 626
         """
         T0 = 0
+        # Index error if enum is used and if reg is used returns all zeros
         expected_result = np.array([0, 321, 325, 625])
-        result = self.ephemeris.lin_fit(test_epochs, test_P_fits, T0, test_tra_or_occ_enum)
+        result = self.ephemeris.lin_fit(test_epochs, test_P_fits, T0, test_tra_or_occ)
+        print(result)
         self.assertTrue(np.allclose(expected_result, result, rtol=1e-05, atol=1e-08))
 
     def test_lin_fit_model(self):
@@ -130,24 +148,33 @@ class TestPrecessionModelEphemeris(unittest.TestCase):
         self.assertIsInstance(self.ephemeris, PrecessionModelEphemeris)
 
     def test_anomalistic_period(self):
-        expected_result = np.array()
-        result = self.ephemeris._anomalistic_period()
-        self.assertTrue(np.allclose(expected_result, result, rtol=1e-05, atol=1e-08))
-        pass
+        expected_result = 1.0915939528522707
+        test_dwdE =  0.000984
+        result = self.ephemeris._anomalistic_period(test_P_fits, test_dwdE)
+        self.assertTrue(expected_result, result)
+        
 
     def test_pericenter(self):
-        expected_result = np.array()
-        result = self.ephemeris._pericenter()
+        # expected_result = np.array([2.62, 2.909296, 2.913232, 2.9152, 2.95456, 2.9644, 2.97424])
+        expected_result = np.array([1.00624,  1.295536, 1.299472, 1.570072, 1.575976, 1.590736, 1.598608])
+        test_dwdE =  0.000984
+        test_W0 =  2.62
+        result = self.ephemeris._pericenter(test_W0, test_dwdE, test_epochs_precession)
+        print(result)
         self.assertTrue(np.allclose(expected_result, result, rtol=1e-05, atol=1e-08))
-        pass
+        
 
     def test_precession_fit(self):
         """Tests that the precession_fit function works.
 
             Creates a numpy.ndarray[int] with the length of the test data
         """
-        expected_result = np.array()
-        result = self.ephemeris.precession_fit(test_epochs, 0, test_P_fits, 0, test_tra_or_occ_enum)
+        # expected_result = np.array([9.33910858e-04, 3.21423111e+02, 3.25788802e+02, 6.25386429e+02,6.31934975e+02, 6.48306322e+02, 6.57037708e+02])
+        expected_result = np.array([-5.76315154e-04, 3.21424452e+02, 3.25790140e+02, 6.25385378e+02, 6.31933923e+02,  6.48305283e+02,  6.57036676e+02])
+        test_W0 =  2.62
+        test_dwdE = 0.000984
+        test_e = 0.00310
+        result = self.ephemeris.precession_fit(test_epochs, 0, test_P_fits, test_dwdE, test_W0, test_e, test_tra_or_occ_enum)
         self.assertTrue(np.allclose(expected_result, result, rtol=1e-05, atol=1e-08))
 
     def test_precession_fit_model(self):
@@ -169,16 +196,16 @@ class TestPrecessionModelEphemeris(unittest.TestCase):
         """
         result = self.ephemeris.fit_model(test_epochs, test_mtts, test_mtts_err, test_tra_or_occ)
         return_data = {
-            'period': 1.0892663209112947,
-            'period_err': 0.002368690041166098,
-            'conjunction_time': 2454515.5241231285,
-            'conjunction_time_err': 0.3467430587812461,
-            'pericenter_change_by_epoch': 4.223712653342504e-06,
-            'pericenter_change_by_epoch_err': 7.742732700893123e-06,
-            'eccentricity': 3.0,
-            'eccentricity_err': 3.0,
-            'pericenter': 3.0,
-            'pericenter_err': 3.0
+            'period': 1.0914233780823739,
+            'period_err': 2.5837552101593316e-06,
+            'conjunction_time': 2454515.5247473116,
+            'conjunction_time_err': 0.0016189004920040013,
+            'pericenter_change_by_epoch': -862310.36579702,
+            'pericenter_change_by_epoch_err':  1933.7369753292073,
+            'eccentricity': 0.5345414535549522,
+            'eccentricity_err': 13588.186664524692,
+            'pericenter': -55020653.47561098,
+            'pericenter_err':589049.1819169023
         }
         self.assertEqual(result['period'], return_data['period'])
         self.assertEqual(result['period_err'], return_data['period_err'])
